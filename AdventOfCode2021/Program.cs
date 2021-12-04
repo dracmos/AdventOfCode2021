b6531eb8cbeb815abb9fe8666f6998fbf09614f4
Casimiro Greco <casimirog@mkinet.net>
2021-12-04 10:56:06 +0100

    Add project files.

---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode2021.Days;
+using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = new Day4();
+            day.GetResult();
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
